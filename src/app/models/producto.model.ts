e49bbd8063a04b7128a28424d25b89ef717335ea
Leoelpre <a22300909@gmail.com>
2026-02-16 13:24:00 -0600

    Agregue comentarios :v

--- src/app/models/producto.model.ts
+++ src/app/models/producto.model.ts
@@ -1,3 +1,7 @@
+
+//Paso numero uno, creamos el modelo del producto, es decir, 
+// la estructura de datos que va a tener cada producto en nuestra aplicación. 
+// Esto nos ayudará a mantener nuestro código organizado y a evitar errores al manipular los datos de los productos.
 export interface Producto {
   id: number;
   nombre: string;
---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,4 @@
+//Paso dos: Creamos el servicio de prodductos, 
+// este servicio se encargará de manejar toda la lógica relacionada con 
+// los productos, como obtener la lista de productos, agregar un nuevo producto,
+//  eliminar un producto, etc.

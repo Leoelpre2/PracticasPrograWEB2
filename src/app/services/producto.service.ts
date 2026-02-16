47b78467a33df3bb055e5b428305b081768dc3a1
Leoelpre <a22300909@gmail.com>
2026-02-16 13:35:16 -0600

    Cree el arreglo de mi producto

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -2,3 +2,19 @@
 // este servicio se encargará de manejar toda la lógica relacionada con 
 // los productos, como obtener la lista de productos, agregar un nuevo producto,
 //  eliminar un producto, etc.
+import {Injectable} from '@angular/core';
+import {Producto} from '../models/producto.model';
+@Injectable({providedIn: 'root'})
+export class ProductsService{
+    private productos: Producto[] = [
+        {
+            id: 1,
+            nombre: 'Producto 1',
+            descripcion: 'Descripción del producto 1',
+            precio: 10.99,
+            imagenUrl: 'https://via.placeholder.com/150',
+            categoria: 'Categoría 1',
+            inStack: true
+        }
+    ];
+}

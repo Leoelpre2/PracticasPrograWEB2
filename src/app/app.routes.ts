6eb1b31acee1182a1a677c6359e6ea79dd6c6ed1
Leoelpre <a22300909@gmail.com>
2026-02-17 10:45:34 -0600

    Modificación del archivo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Catalogo } from './components/catalogo/catalogo';
+export const routes: Routes = [
+    {path: "",component:Catalogo},
+    {path: "", redirectTo:''}
+];

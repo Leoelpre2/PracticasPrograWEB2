9ebfcd3b5de39d71aeead9229041c1c44a9543b2
Leoelpre <a22300909@gmail.com>
2026-02-17 10:37:36 -0600

    Creación de componente de catalogo

--- src/app/components/catalogo/catalogo.ts
+++ src/app/components/catalogo/catalogo.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ProductoCardComponent } from '../producto-card/producto-card.component.ts/producto-card.component.ts';
+import { ProductsService } from '../../services/producto.service';
+import { Producto } from '../../models/producto.model';
 @Component({
   selector: 'app-catalogo',
-  imports: [],
+  standalone: true,
+  imports: [ProductoCardComponent],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
 })
 export class Catalogo {
-
+  producto:Producto[] = [];
+  constructor(private productsService:ProductsService){
+    this.producto = this.productsService.getAll();
+  }
 }
--- src/app/components/producto-card/producto-card.component.ts/producto-card.component.ts.ts
+++ src/app/components/producto-card/producto-card.component.ts/producto-card.component.ts.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import {Producto} from '../models/producto.model';
 @Component({
-  selector: 'app-producto-card.component.ts',
-  imports: [],
-  templateUrl: './producto-card.component.ts.html',
-  styleUrl: './producto-card.component.ts.css',
+  selector: 'app-producto-card', //Lo que llamemos squi se va a ver en la vista
+  standalone: true,
+  templateUrl: './producto-card.component.html', //Plantilla 
+  styleUrl: './producto-card.component.css',
 })
-export class ProductoCardComponentTs {
-
+export class ProductoCardComponent {
+  @Input() producto!: Producto; //Recibe un producto como entrada
 }
